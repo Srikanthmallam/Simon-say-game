b159f5afbf8598b2a4287b344c479a82ffb93692
srikanth mallam <srikanthmallam0@gmail.com>
2023-08-24 17:54:30 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+let gameseq = [];
+let userseq = [];
+
+let btns = ["btn1","btn2","btn3","btn4"];
+
+let started = false;
+let level = 0;
+
+let highscore = document.querySelector("#highscore");
+
+
+let levelindi = document.querySelector("h2");
+
+document.addEventListener("keypress",function(){
+    if(started == false){
+        console.log("game started");
+        started = true;
+
+        levelUp();
+    }
+});
+
+function btnflash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);
+};
+
+function levelUp(){
+    userseq=[];
+    level++;
+    levelindi.innerText = `Level ${level}`;
+
+    let randomidx = Math.floor(Math.random() * 3 );
+    let btn = btns[randomidx];
+    let randombtn = document.querySelector(`.${btn}`);
+
+    gameseq.push(btn);
+    console.log(gameseq);
+    btnflash(randombtn);
+}
+
+function checkseq(idx){
+    if(userseq[idx]===gameseq[idx]){
+        if(userseq.length == gameseq.length){
+            setTimeout(levelUp,1000);
+        }
+    }else{
+        levelindi.innerHTML=`Game Over! &nbspYour score was <b>${level}</b> <br> press any key to strat again`;
+        reset();
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "white";
+        },150)
+    }
+
+}
+
+function btnpress(){
+    let btn = this;
+    btnflash(btn);
+
+    let userbtn = btn.getAttribute("id");
+    userseq.push(userbtn);
+    console.log(userseq)
+
+    checkseq(userseq.length-1);
+}
+
+let allbtns = document.querySelectorAll(".btn")
+for(btn of allbtns){
+    btn.addEventListener("click",btnpress);
+}
+
+function reset(){
+    highscore.innerText = `High score is : ${level}`;
+    started = false;
+    gameseq=[];
+    userseq=[];
+    level=0;
+}
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Simon Says Game
+
+Classic memory game. Repeat color & sound sequences. Test your memory and have fun!
+
+## How to Play
+
+1. Visit the game [here](https://your-game-url.com).
+2. Press "Start" to begin.
+3. Repeat the sequence by clicking colors.
+4. Game gets harder as you play.
+5. Make a mistake, game ends.
+
+## Features
+
+- Classic Simon Says gameplay.
+- Random sequences.
+- User-friendly interface with sounds.
+- Increasing difficulty.
+- Score tracking.
+
+## Tech
+
+- HTML, CSS, JavaScript.
+- Audio for sound effects.
+
+## Contribute
+
+1. Fork repository.
+2. Create feature branch.
+3. Make changes, test.
+4. Commit and push.
+5. Create pull request.
+
+## License
+
+MIT License. See [LICENSE](LICENSE).
+
+## Acknowledgements
+
+Sounds from [SoundBible](https://www.soundbible.com/).
